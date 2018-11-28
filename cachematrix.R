d77e1779b12bc645a19ec6f09e9edf4685165d45
QuinlanSievers <45438948+QuinlanSievers@users.noreply.github.com>
2018-11-28 17:13:14 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,23 +1,23 @@
-makeCacheMatrix <- function(x = matrix()) {
-    i <- NULL
-    set <- function(y) {
-        x <<- y
-        i <<- NULL
+makeCacheMatrix <- function(x = matrix()) { #define makeCacheMatrix function, define x as empty matrix
+    i <- NULL #set i as null
+    set <- function(y) { #define set function
+        x <<- y #set x as y in global environment
+        i <<- NULL #set i as null in global environment
     }
-    get <- function() x
-    setinverse <- function(inverse) i <<- inverse
-    getinverse <- function() i
-    list(set = set, get = get,setinverse = setinverse, getinverse = getinverse)
+    get <- function() x #define get function (returns input matrix)
+    setinverse <- function(inverse) i <<- inverse #define setinverse function (sets the inverse)
+    getinverse <- function() i #define getinverse function (returns inverse of matrix)
+    list(set = set, get = get,setinverse = setinverse, getinverse = getinverse) #Generates output list of four functions
 }
 
-cacheinverse <- function(x, ...) {
-    i <- x$getinverse()
-    if(!is.null(i)) {
+cacheinverse <- function(x, ...) { #define cacheinverse function
+    i <- x$getinverse() #define i as output of getinverse
+    if(!is.null(i)) { #conditional stating that if getinverse is not NULL, then to return the inverse of the matrix
         message("getting cached data")
         return(i)
     }
-    data <- x$get()
-    i <- solve(data, ...)
-    x$setinverse(i)
-    i
+    data <- x$get() #define data as the input matrix
+    i <- solve(data, ...) #define i as inverse of input matrix
+    x$setinverse(i) #set inverse value as i in the global environment?
+    i #return i
 }
